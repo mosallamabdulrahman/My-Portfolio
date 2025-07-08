c8a92f5276f26cd0f4e1627b5bf743032c5348fd
AbdulrahmanMosallam <mosallamabdulrahman@gmail.com>
2025-07-08 03:09:23 +0300

    initial commit

---
+++ src/components/2-hero/Hero.jsx
@@ -0,0 +1,60 @@
+import Lottie from "lottie-react";
+import "./hero.css";
+import devAnimation from "../../animation/dev.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="./myImageCrop.png"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          FrontEnd Developer, Wordpress Developer.
+        </motion.h1>
+
+        <p className="sub-title">
+          I’m Abdulrahman Rafat, a FrontEnd Developer & Wordpress Developer Work In <a href="https://mosalam.com/">Mosalam</a> Company
+        </p>
+
+        <div className="all-icons flex">
+          <a href="https://github.com/mosallamabdulrahman"><div className="icon icon-github"></div></a>
+          <a href="https://www.linkedin.com/in/abdulrahman-rafat-998760316/"><div className="icon icon-linkedin"></div></a>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/3-main/myProjects.js
@@ -0,0 +1,78 @@
+export const myProjects = [
+  { projectTitle: "Tailwind css project",
+    category: ["css"],
+    imgPath: "./images/1.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/Tailwind-Builder/",
+    linkGithub: "https://github.com/mosallamabdulrahman/Tailwind-Builder"
+    },
+  {
+    projectTitle: "Tailwind css project",
+    category: ["css"],
+    imgPath: "./images/2.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/Planet-Website/",
+    linkGithub: "https://github.com/mosallamabdulrahman/Planet-Website"
+  },
+  {
+    projectTitle: "Css Project",
+    category: ["css"],
+    imgPath: "./images/3.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/AMI/",
+    linkGithub: "https://github.com/mosallamabdulrahman/AMI"
+  },
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/4.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/JavaScript-Game-Rock-Paper-Scissors/",
+    linkGithub: "https://github.com/mosallamabdulrahman/JavaScript-Game-Rock-Paper-Scissors"
+  },
+  { projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/5.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/Work-With-Github-Api/",
+    linkGithub: "https://github.com/mosallamabdulrahman/Work-With-Github-Api"
+  },
+
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/6.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/Strong-Password-Generator/",
+    linkGithub: "https://github.com/mosallamabdulrahman/Strong-Password-Generator"
+  },
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/7.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/Guess-The-Word-Game/",
+    linkGithub: "https://github.com/mosallamabdulrahman/Guess-The-Word-Game"
+  },
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/8.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/Weather-App/",
+    linkGithub: "https://github.com/mosallamabdulrahman/Weather-App"
+  },
+  {
+    projectTitle: "TypeScript project",
+    category: ["ts"],
+    imgPath: "./images/9.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/To-Do-App-With-Local-Storage/",
+    linkGithub: "https://github.com/mosallamabdulrahman/To-Do-App-With-Local-Storage"
+  },
+  {
+    projectTitle: "React & MUI",
+    category: ["react"],
+    imgPath: "./images/10.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/Todos-by-React/",
+    linkGithub: "https://github.com/mosallamabdulrahman/Todos-by-React"
+  },
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/11.jpg",
+    liveLink: "https://mosallamabdulrahman.github.io/hangman-game/",
+    linkGithub: "https://github.com/mosallamabdulrahman/hangman-game"
+  },
+];
